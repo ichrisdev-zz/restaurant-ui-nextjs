fe588ad4c012a8f9057500eea9151eb5f6fcc6b8
Nicolás Morales <nico.moralesh@alumnos.duoc.cl>
2022-10-26 23:25:57 -0300

    Home - Landing
    Co-authored-by: ichrisdev <ichrisdev@users.noreply.github.com>

---
+++ components/Landing/find-us/FindUs.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import React from 'react';
+import images from '../../../constants/images';
+
+
+const FindUs = () => (
+  <div className='app__bg app__wrapper section__padding' id="contact">
+    <div className='app__wrapper_info'>
+      <div style={{ marginBottom: '1rem' }}>
+        <p className='p__cormorant'>Contacto</p>
+        <Image src={images.spoon} alt="spoon" className='spoon__img' />
+      </div>
+      <h1 className='headtext__cormorant' style={{ marginBottom: '3rem' }}>Encuentranos</h1>
+      <div className='app__wrapper-content'>
+        <p className='p__opensans'>Vicuña Mackenna 4917, San Joaquín, Región Metropolitana</p>
+        <p className='p__cormorant' style={{ color: '#DCCA87', margin: '2rem 0'}}>Horarios:</p>
+        <p className='p__opensans'>Lun - Vie: 10:00 AM - 22:00 PM</p>
+        <p className='p__opensans'>Sab - Dom: 11:00 AM - 19:00 PM</p>
+      </div>
+      <button className='custom__button' style={{ marginTop: '2rem'}}>Visitanos</button>
+
+    </div>
+
+    <div className='app__wrapper_img'>
+      <Image src={images.findus} alt="findus" />
+    </div>
+  </div>
+);
+
+export default FindUs;
---
+++ components/Landing/laurels/Laurels.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import images from '../../../constants/images';
+import data from '../../../constants/data';
+
+import '../../../styles/modules/Home/Laurels.module.scss';
+import Image from 'next/image';
+
+const Laurels = () => (
+  <div className='app__bg app__wrapper section__padding' id='awards'>
+    <div className='app__wrapper_info'>
+      <div style={{ marginBottom: '1rem' }}>
+        <p className='p__cormorant'>Premios y Reconocimientos</p>
+        <Image src={images.spoon} alt="spoon" className='spoon__img' />
+      </div>
+      <h1 className='headtext__cormorant'>Nuestros Logros</h1>
+      <div className='app__laurels_awards'>
+        
+        {data.awards && data.awards.map(({id, imgUrl, title,subtitle}: any) => {
+                return(
+                    <div className='app__laurels_awards-card'>
+                      <Image src={imgUrl} alt="award" />
+                      <div className='app__laurels_awards-card_content'>
+                        <p className='p__cormorant' style={{ color: '#DCCA87' }}>{title}</p> 
+                        <p className='p__cormorant'>{subtitle}</p> 
+                      </div>                   
+                    </div>                
+                )
+              })
+            }
+      </div>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <Image src={images.laurels} alt="laurels"/>            
+    </div>    
+  
+  
+  </div>
+);
+
+export default Laurels;
