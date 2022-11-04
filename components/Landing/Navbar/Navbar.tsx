3267c16ae6eaaaaeb28ca744a52a6ed5b82edcac
ichrisdev <chrmesina@icloud.com>
2022-11-04 00:12:42 -0300

    feat: added pm2 restart nextjs app

--- components/Landing/Navbar/Navbar.tsx
+++ components/Landing/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <Image src={gericht} alt="app__logo" />
       </div>
       <ul className="app__navbar-links">
-        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#home">Test</a></li>
         <li className="p__opensans"><a href="#about">About</a></li>
         <li className="p__opensans"><a href="#menu">Menu</a></li>
         <li className="p__opensans"><a href="#awards">Awards</a></li>
